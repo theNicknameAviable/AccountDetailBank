e305f873c44ebb1da3735f1c970f6226d9366884
Raul Bautista <rbmolina27@gmail.com>
2022-03-14 20:37:13 +0100

    initial commit

---
+++ AccountDetailBank/AccountDetailBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountDetailBank
+//
+//  Created by Raul Bautista on 14/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
